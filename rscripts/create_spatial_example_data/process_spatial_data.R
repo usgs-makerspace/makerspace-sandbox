871efb23ac28b451e490c2a496e2178ef5c1cac4
wdwatkins <wwatkins@usgs.gov>
2019-05-03 16:07:00 -0700

    playing with simplification a bit.  rmapshaper::ms_simplify seems like the way to go, unless we use js tools like in water use

--- rscripts/create_spatial_example_data/process_spatial_data.R
+++ rscripts/create_spatial_example_data/process_spatial_data.R
@@ -7,6 +7,12 @@ gfdb <- "cache/GF_nat_reg.gdb"
 
 # Clean up HRU spatial data
 hru_loaded <- read_sf(gfdb, "nhru")
+#there are layers that list point/line errors,
+#not sure if these are useful?
+#line_errors <- read_sf(gfdb, "T_1_LineErrors")
+pryr::object_size(hru_loaded$Shape)
+#shape size is yuge
+#all geometries are simple, but some are invalid
 hru_valid <- st_make_valid(hru_loaded)
 hru <- hru_valid %>% 
   select(hru_id_nat) %>%
@@ -14,6 +20,16 @@ hru <- hru_valid %>%
 
 st_agr(hru) <- "constant"
 
+#TODO:
+#intersect HRUs with a state so can work with political
+#boundaries and get a sense of scale
+#probably need to get them in the same projection first
 
+#this seem like best option that preserves topology
 # Might use: rmapshaper::ms_simplify()
-
+hru_sample <- hru_loaded$Shape[1:1000]
+hru_sample_simple <- ms_simplify(hru_sample, keep=0.01)
+pryr::object_size(hru_sample)
+pryr::object_size(hru_sample_simple)
+plot(hru_sample)
+plot(hru_sample_simple)
