ad3f1d59190774f30b8c45b38198169700e4f961
wdwatkins <wwatkins@usgs.gov>
2019-05-06 17:41:36 -0700

    simplifying hrus in one state

--- rscripts/create_spatial_example_data/process_spatial_data.R
+++ rscripts/create_spatial_example_data/process_spatial_data.R
@@ -7,18 +7,6 @@ gfdb <- "cache/GF_nat_reg.gdb"
 
 # Clean up HRU spatial data
 hru_loaded <- read_sf(gfdb, "nhru")
-#there are layers that list point/line errors,
-#not sure if these are useful?
-#line_errors <- read_sf(gfdb, "T_1_LineErrors")
-pryr::object_size(hru_loaded$Shape)
-#shape size is yuge
-#all geometries are simple, but some are invalid
-hru_valid <- st_make_valid(hru_loaded)
-hru <- hru_valid %>% 
-  select(hru_id_nat) %>%
-  st_sf()
-
-st_agr(hru) <- "constant"
 
 #intersect HRUs with a state so can work with political
 #boundaries and get a sense of scale
@@ -34,15 +22,29 @@ ohio_projected <- st_transform(ohio_sf, crs = st_crs(hru_loaded))
 hru_ohio_intersect <- st_intersection(ohio_projected, hru_loaded)
 plot(hru_ohio_intersect$geometry) #all hru inside ohio
 
-
 #this seem like best option that preserves topology
 # Might use: rmapshaper::ms_simplify()
-hru_sample <- hru_loaded$Shape[1:1000]
-hru_sample_simple <- ms_simplify(hru_sample, keep=0.01)
-pryr::object_size(hru_sample)
-pryr::object_size(hru_sample_simple)
-plot(hru_sample)
-plot(hru_sample_simple)
+library(rmapshaper)
+ohio_simple <- ms_simplify(hru_ohio_intersect, keep=0.005)
+pryr::object_size(hru_ohio_intersect)
+pryr::object_size(ohio_simple)
+plot(ohio_simple$geometry)
+
+#there are layers that list point/line errors,
+#not sure if these are useful?
+#line_errors <- read_sf(gfdb, "T_1_LineErrors")
+pryr::object_size(hru_loaded$Shape)
+#shape size is yuge
+#all geometries are simple, but some are invalid
+hru_valid <- st_make_valid(hru_loaded)
+hru <- hru_valid %>% 
+  select(hru_id_nat) %>%
+  st_sf()
+
+st_agr(hru) <- "constant"
+
+
+
 
 #TODO: write out to topojson or however is used by 
 #water use viz
