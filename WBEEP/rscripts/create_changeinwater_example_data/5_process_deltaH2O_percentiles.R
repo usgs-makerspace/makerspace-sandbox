ee7799fc2e2bd1f8215d9f27dc4d7fa0ce190a26
Platt <lplatt@usgs.gov>
2019-06-21 10:46:34 -0500

    fix percentiles that were > 1 (use dplyr instead of data.table, see #74 for more info)

--- WBEEP/rscripts/create_changeinwater_example_data/5_process_deltaH2O_percentiles.R
+++ WBEEP/rscripts/create_changeinwater_example_data/5_process_deltaH2O_percentiles.R
@@ -16,7 +16,8 @@ quantile_nodups <- quantile_df %>%
   group_by(HRU) %>%
   # Keep row if it is not equal to the following one
   # `default` returns TRUE so that it keeps the last row
-  filter(lead(stat_value, default=TRUE) != stat_value)
+  filter(lead(stat_value, default=TRUE) != stat_value) %>% 
+  ungroup()
 
 # Format quantiles to be used in `cut`
 #   Don't know how to do this using data.table, so used dplyr
@@ -50,16 +51,16 @@ find_quantile_group <- function(value, breaks, labels) {
   cut(value, unlist(unique(breaks)), unlist(unique(labels))[-1])
 }
 
-# Now merge values with quantiles in order to figure out snowpack %iles
+# Now merge values with quantiles in order to figure out %iles
 value_dt_stat <- merge(value_dt, quantile_summary_dt, all.x = TRUE, by="HRU")
 
-##### 5/20 - when I do the full set, some are not using the right stats
-# when I use `find_quantile_group` for just one HRU it works.
-# the merge above is giving me problems about no matching col names when I 
-# don't specify "by" even though there are
-# The result is that there are some totS_per that are >1
-value_dt_stat[, stat_to_use := find_quantile_group(deltaH2O, stat_value_list, stat_type_list),
-              by = list(HRU)]
+# Determine which is the highest percentile that would be appropriate to use for interpolation
+value_dt_stat <- value_dt_stat %>% 
+  group_by(HRU) %>%
+  mutate(stat_to_use = find_quantile_group(deltaH2O, stat_value_list, stat_type_list)) %>%
+  ungroup() %>% 
+  as.data.table()
+
 value_dt_stat[,c("stat_type_list", "stat_value_list") := NULL] # Clean up and delete unneeded columns
 
 # Now merge interp info with values and actually do linear interpolation to figure out the
@@ -69,8 +70,6 @@ per_dt <- merge(value_dt_stat, quantile_dt_interp_info, by.x = c("HRU", "stat_to
 per_dt[, deltaH2O_per := 
          (((stat_type1 - stat_type0) / (stat_value1 - stat_value0)) * (deltaH2O - stat_value0)) + stat_type0]
 
-
-################ End up with >1 percentiles... HMMMMM
 
 # Clean up to just be left with HRU, Date, Runoff_va, and Runoff_per
 per_dt[, c("stat_to_use", "stat_name", "stat_value", "stat_type0", "stat_type1", "stat_value0", "stat_value1") := NULL]
