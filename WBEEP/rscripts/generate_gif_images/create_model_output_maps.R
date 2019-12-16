6cdd853b19fe64620fcf4ae6046bb2128b4fec26
Platt <lplatt@usgs.gov>
2019-12-16 15:57:40 -0600

    switch from conus_sf to hru_sf for topojson data

--- WBEEP/rscripts/generate_gif_images/create_model_output_maps.R
+++ WBEEP/rscripts/generate_gif_images/create_model_output_maps.R
@@ -1,5 +1,5 @@
 
-create_maps <- function(data_downloaded, conus_sf, canada_sf, mexico_sf) {
+create_maps <- function(data_downloaded, hru_sf, canada_sf, mexico_sf) {
   
   dates <- gsub("model_output_categorized_|.csv", "", basename(data_downloaded))
   
@@ -8,14 +8,14 @@ create_maps <- function(data_downloaded, conus_sf, canada_sf, mexico_sf) {
     map_data_i <- readr::read_csv(data_downloaded[i]) %>% 
       select(hru_id_nat, value) %>% 
       filter(value != "Undefined")
-    map_fns[i] <- make_map(dates[i], conus_sf, canada_sf, mexico_sf, map_data_i)
+    map_fns[i] <- make_map(dates[i], hru_sf, canada_sf, mexico_sf, map_data_i)
   }
   
   return(map_fns)
 }
 
 
-make_map <- function(date, conus_sf, canada_sf, mexico_sf, map_data) {
+make_map <- function(date, hru_sf, canada_sf, mexico_sf, map_data) {
 
   map_colors_vec <- c("#967a4a", "#BDAD9D", "#C8D3BA", "#337598", "#1C2040")
   names(map_colors_vec) <- c("very low", "low", "average", "high", "very high")
@@ -23,14 +23,14 @@ make_map <- function(date, conus_sf, canada_sf, mexico_sf, map_data) {
                               color = c("#967a4a", "#BDAD9D", "#C8D3BA", "#337598", "#1C2040"),
                               stringsAsFactors = FALSE)
   
-  conus_data_sf <- left_join(conus_sf, map_data, by = "hru_id_nat") %>% 
-    filter(!is.na(value)) %>% # there are some HRUs in conus_sf that aren't in the data
+  hru_data_sf <- left_join(hru_sf, map_data, by = "hru_id_nat") %>% 
+    filter(!is.na(value)) %>% # there are some HRUs in hru_sf that aren't in the data
     left_join(map_colors_df, by = "value")
 
   # Base plot is much faster to save than a ggplot2
   map_fn <- sprintf("WBEEP/rscripts/generate_gif_images/img/map_%s.png", date)
   png(map_fn, width = 11, height = 8, units="in", res=300)
-  plot(st_geometry(conus_data_sf), col = conus_data_sf$color, border=NA, 
+  plot(st_geometry(hru_data_sf), col = hru_data_sf$color, border=NA, 
        axes=FALSE, main = date)
   # CANADA/MEXICO not quite right yet.
   #plot(st_geometry(canada_sf), col = "black", add=TRUE)
@@ -41,17 +41,17 @@ make_map <- function(date, conus_sf, canada_sf, mexico_sf, map_data) {
 
 }
 
-load_conus_shape <- function(fn, proj) {
+load_hru_shape <- function(fn, proj) {
   
-  topo_data_conus <- topojson_read(fn)
-  conus_sf <- st_as_sf(topo_data_conus)
-  st_crs(conus_sf) <- proj
+  topo_data_hru <- topojson_read(fn)
+  hru_sf <- st_as_sf(topo_data_hru)
+  st_crs(hru_sf) <- proj
   
   # No longer need, but want to keep record.
   # Self-intersections were causing downstream problems with summarizing for a dissolve
-  # conus_sf_valid <- st_make_valid(conus_sf) # Needs pkg `lwgeom`
+  # hru_sf_valid <- st_make_valid(hru_sf) # Needs pkg `lwgeom`
   
-  return(conus_sf)
+  return(hru_sf)
 }
 
 create_country_overlays <- function(country, proj) {
