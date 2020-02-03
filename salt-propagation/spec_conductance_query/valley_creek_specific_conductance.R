dd0b9e4a774b68ab028c32cf3ce4849197fd258b
Hines <mhines@usgs.gov>
2020-02-03 13:57:30 -0600

    script to get Valley Creek data and create simple plots to show spec cond spike

---
+++ salt-propagation/spec_conductance_query/valley_creek_specific_conductance.R
@@ -0,0 +1,39 @@
+library(dataRetrieval)
+sites <- whatNWISsites(bBox=c(-75.702667,40.045752,-75.428352,40.109325),
+                       parameterCd=("00095"),
+                       hasDataTypeCd="uv")
+
+# get data for Winter Storm (16-20 Jan 2019) https://www.wpc.ncep.noaa.gov/storm_summaries/event_reviews.php?YYYYMMDD=20190120 
+# Valley Creek at PA Turnpike Br near Valley Forge - 01473169
+parameterCd <- "00095"
+startDate <- "2019-01-16"
+endDate <- "2019-01-20"
+dfWide <- readNWISuv("01473169", "00095",
+                     startDate, endDate)
+dfWide <- dfWide[1:4]
+colnames(dfWide) <- c("agency","station","dateTime","value")
+
+par(mar=c(5,5,5,5)) #sets the size of the plot window
+plot(dfWide$dateTime, dfWide$value,
+     ylab="spec conductance",xlab="dateTime" )
+
+
+# get data for Winter Storm (27-30 Jan 2019) https://www.wpc.ncep.noaa.gov/storm_summaries/event_reviews.php?YYYYMMDD=20190130
+# Valley Creek at PA Turnpike Br near Valley Forge - 01473169
+parameterCd <- "00095"
+startDate <- "2019-01-27"
+endDate <- "2019-01-30"
+dfWide <- readNWISuv("01473169", "00095",
+                     startDate, endDate)
+dfWide <- dfWide[1:4]
+colnames(dfWide) <- c("agency","station","dateTime","value")
+
+par(mar=c(5,5,5,5)) #sets the size of the plot window
+plot(dfWide$dateTime, dfWide$value,
+     ylab="spec conductance",xlab="dateTime" )
+
+
+
+
+
+
